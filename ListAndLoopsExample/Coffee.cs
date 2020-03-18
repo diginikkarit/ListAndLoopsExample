d544edeb485f18b74a8fc0f145f504f190a8f893
mustohe <hmustone18@student.oulu.fi>
2020-03-18 10:25:38 +0200

    Add project files.

---
+++ ListAndLoopsExample/Coffee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListAndLoopsExample
+{
+
+    //kahvit
+    //-merkki, hinta, paahto,maahantuoja
+    //constructorit ilman parametreja, pelkällä nimellä, kaikilla atribuuteilla
+
+    class Coffee
+    {
+        public string brand;        
+        public decimal price;        
+        public int roast;
+        public string importer;
+        public Coffee()
+        {
+
+        }
+
+        public Coffee(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public Coffee(string brand, decimal price, int roast, string importer)
+        {
+            this.brand = brand;
+            this.price = price;
+            this.roast = roast;
+            this.importer = importer;
+        }
+    }
+}
---
+++ ListAndLoopsExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListAndLoopsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paketti1 = new Coffee("Presidentti");
+            var paketti2 = new Coffee("Kulta Katriina");
+            var paketti3 = new Coffee("Brazil");
+            var paketti4 = new Coffee("Löfberg");
+
+            List<Coffee> coffees = new List<Coffee>();
+            coffees.Add(paketti1);
+            coffees.Add(paketti2);
+            coffees.Add(paketti3);
+            coffees.Add(paketti4);
+
+           // Console.WriteLine(coffees[2].brand);
+
+            foreach (var paketti in coffees)
+            {
+                Console.WriteLine($"Merkki {paketti.brand}");
+            }
+
+            for (int i = 0; i < coffees.Count; i++)
+            {
+                Console.WriteLine($"{i}. Merkki : {coffees[i].brand}");
+            }
+
+            //Company -luokka. nimi, yhteyshenkilö(luokkaa person), maa
+            //person -luokka etunimi, sukunimi, puhelinnumero, email
+            //DataHandler -luokka. list<cofee>, list<person>
+            //metodi joka kysyy kahvin merkin, hinnan,roast -> luo näistä instanssin Coffee -> lisää ko. instanssin listaan.
+        }
+    }
+
+
+    //kahvit
+    //-merkki, hinta, paahto,maahantuoja
+    //constructorit ilman parametreja, pelkällä nimellä, kaikilla atribuuteilla
+
+
+}
