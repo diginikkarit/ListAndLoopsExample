d68d8ef29190a75fead7667e7854e7b4b2303071
Heikki Mustonen <hmustone18@student.oulu.fi>
2020-04-21 13:37:36 +0300

    Cleaninig up.

--- ListAndLoopsExample/Program.cs
+++ ListAndLoopsExample/Program.cs
@@ -17,28 +17,7 @@ namespace ListAndLoopsExample
             MainMenu mainMenu = new MainMenu(dh);
             mainMenu.InitializeMainMenu();
 
-            //Testing Person stuff
-            //dh.AddPersonToList();
-            //dh.PrintPersonList();
-            //Console.ReadKey();
-            //Console.Clear();
-            //Console.WriteLine("Valitse henkilö");
-            //var person = dh.SelectPersonFromList();
-            //Console.WriteLine("\n\n");
-            //Console.WriteLine($"Valittu : {person.firstName} {person.lastName} {person.email}");
 
-            //Testing Company stuff
-            //dh.AddNewCompanyToList();
-            //dh.PrintCompanyList();
-            //Console.ReadKey();
-            //Console.Clear();
-            //Company company = dh.SelectCompanyFromList();
-            //Console.WriteLine($"Valittu {company.name} ");
-
-   
-            //testing coffee things.
-            //dh.AddNewCoffeeToList();
-            //dh.PrintCoffeeList();
                                  
         }
     }
